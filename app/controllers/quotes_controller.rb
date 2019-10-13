5b0b2dc5f7530fea8db1b68ff8f6f3cdd43191dc
Dan Pham <dan.p.pham97@gmail.com>
2019-10-13 22:31:59 +0000

    error on about page #nomethod

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,6 @@
 class QuotesController < ApplicationController
     def index 
         @quote = Quote.order("Random()").first
-    end
-
-    def new
-        @quote = Quote.new
     end 
 
     def create
